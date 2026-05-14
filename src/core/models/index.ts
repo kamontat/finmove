55723ed90efb77ec26e51bf0884cba89236d38aa
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-05-15 01:11:03 +0700

    chore: sort barrel exports by import path

--- src/core/models/index.ts
+++ src/core/models/index.ts
@@ -3,6 +3,6 @@ export { AccountType } from "./account";
 export type { Expense, ExpenseOwnerSplit } from "./expense";
 export { SplitType } from "./expense";
 export type { Owner } from "./owner";
-export type { Tag } from "./tag";
 export type { CurrencyConfig, Settings } from "./settings";
+export type { Tag } from "./tag";
 export type { Trip } from "./trip";
--- src/core/validators/index.ts
+++ src/core/validators/index.ts
@@ -1,5 +1,5 @@
+export { validateTag } from "./tag";
 export { validateAccounts } from "./validateAccounts";
 export { validateExpenses } from "./validateExpenses";
 export { validateOwners } from "./validateOwners";
 export { validateSettings } from "./validateSettings";
-export { validateTag } from "./tag";
